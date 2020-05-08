04fdf29bc103614dceca039af8a823af37fc22a1
Shashank Kumar Drolia <49876041+shashu421@users.noreply.github.com>
2020-05-08 14:56:59 +0530

    Changed to local database

--- products/app.js
+++ products/app.js
@@ -17,12 +17,15 @@ app.use(express.urlencoded({
 
 app.use('/products',addnewprodroutes);
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true});
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("Connected Database Successfully");
-});
+//database connection
+mongoose.connect("mongodb://localhost/humourbaba");
+
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true});
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log("Connected Database Successfully");
+// });
 
 app.listen(4000,function(){
     console.log("Server has started on port 4000");
